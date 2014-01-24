4b5570025182e85a12640aff875943cbed251fd2
catLee <leemiyinghao@gmail.com>
2014-01-24 15:36:17 +0900

    Reload Function and Volume Fix complete. Version 0.12 'ISUZU' release.

--- src/com/longCat/nyaFurnace/main.java
+++ src/com/longCat/nyaFurnace/main.java
@@ -1,13 +1,20 @@
 package com.longCat.nyaFurnace;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class main extends JavaPlugin{
-	String interduce = ChatColor.GREEN + "nyaFurnace 0.1 Beta\n" + ChatColor.DARK_GRAY + "By Tech. Grp.,LongCat City Gov.";
+	String interduce = ChatColor.GREEN + "nyaFurnace 0.12 'ISUZU'\n" + ChatColor.DARK_GRAY + "By Tech. Grp.,LongCat City Gov.";
+	Plugin plugin = this;
 	public void onEnable(){
 		this.saveDefaultConfig();
 		Sound sound;
@@ -20,9 +27,10 @@ public final class main extends JavaPlugin{
 		}
 		int repeat = this.getConfig().getInt("repeat");
 		int interval = this.getConfig().getInt("interval");
-		float volume = 3.0F;
+		float volume = 1.0F;
 		//TODO: 
-		//	Adjust the volume,3.0F is too big. 
+		//	Adjust the volume,3.0F is too big.
+		//		I think 1.0F is enough,that is the biggest default value.
 		float pitch;
 		if(
 			sound.equals(Sound.NOTE_BASS) ||
@@ -45,11 +53,35 @@ public final class main extends JavaPlugin{
 			getLogger().log(Level.SEVERE, "The \"interval\" config value is not positive,set to 1");
 			interval = 1;
 		}
-		if(pitch < 1){
-			getLogger().log(Level.SEVERE, "The \"pitch\" config value is not positive,set to 1");
-			pitch = 1;
+		if(pitch < 0.5F){
+			getLogger().log(Level.SEVERE, "The \"pitch\" config value is not positive,set to 6");
+			pitch = 0.5198F;
 		}
 		getServer().getPluginManager().registerEvents(new furnaceSmeltEmptyListener(sound, repeat, interval,volume,pitch), this);
 		getServer().getPluginManager().registerEvents(new furnaceFuelEmptyListener(sound, repeat, interval,volume,pitch), this);
+	}
+	final public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("nyafurnace")){
+			if(args.length == 1){
+				if(args[0].equalsIgnoreCase("reload")){
+					if(sender.hasPermission("nyaFurnace.reload")){
+						Bukkit.getPluginManager().disablePlugin(plugin);
+						sender.sendMessage(ChatColor.YELLOW + "nyaFurnace disable");
+						Timer timer = new Timer();
+						timer.schedule(new TimerTask(){
+							public void run(){
+								Bukkit.getPluginManager().enablePlugin(plugin);
+								sender.sendMessage(ChatColor.YELLOW + "nyaFurnace enable");
+							}
+						}, 200);
+					}
+					return true;
+				}
+			}
+			sender.sendMessage(interduce);
+			sender.sendMessage(ChatColor.YELLOW + "Usage: /nyaFurnace reload");
+			return true;
+		}
+		return false;
 	}
 }
