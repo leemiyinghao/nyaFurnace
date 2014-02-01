6d64d28291d010c543f5ab7f2e3e85529a2b466a
catLee <leemiyinghao@gmail.com>
2014-02-02 00:25:58 +0900

    Fix bug:Furnace nya when there is no fuel in the furnace but furnace is still burning.

--- src/com/longCat/nyaFurnace/furnaceFuelEmptyListener.java
+++ src/com/longCat/nyaFurnace/furnaceFuelEmptyListener.java
@@ -2,7 +2,14 @@ package com.longCat.nyaFurnace;
 //import java.util.Timer;
 //import java.util.TimerTask;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.bukkit.Location;
+import org.bukkit.Material;
+//import org.bukkit.World;
+//import org.bukkit.block.Block;
+//import org.bukkit.block.Furnace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.FurnaceBurnEvent;
@@ -22,15 +29,27 @@ public final class furnaceFuelEmptyListener implements Listener {
 	@EventHandler
 	public void smelt(final FurnaceBurnEvent event) throws InterruptedException {
 		if(event.getFuel().getAmount() <= 1){
-			Location loc = event.getBlock().getLocation();
-			for(int i = 0;i<repate;i++){
-				event.getBlock().getWorld().playSound(loc, sound,volume,pitch);
-				//event.getBlock().getWorld().playEffect(loc, type, data);
-				//TODO:
-				//	Find out what the type and data should be if I want to call a note partical effect.
-				//	I don't understand why they don't put this in Javadoc.
-				Thread.sleep(interval);
-			}
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask(){
+				public void run(){
+					if(event.getBlock().getType().equals(Material.FURNACE)){//If furnace cool down
+						Location loc = event.getBlock().getLocation();
+						try {
+							for(int i = 0;i<repate;i++){
+							event.getBlock().getWorld().playSound(loc, sound,volume,pitch);
+							//event.getBlock().getWorld().playEffect(loc, type, data);
+							//TODO:
+							//	Find out what the type and data should be if I want to call a note partical effect.
+							//	I don't understand why they don't put this in Javadoc.
+							Thread.sleep(interval);
+							}
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}, (event.getBurnTime()+1) * 50);
 		}
 	}
 }
